be364420885705606d07ec80805c9f096e85e7cd
Nicholas Willard <nswillard@hotmail.com>
2018-08-26 22:44:17 -0500

    Adding my raw versions of the first 3 scripts.

---
+++ Install-Steam.ps1
@@ -0,0 +1,43 @@
+﻿function Install-Steam {
+    param(
+    )
+    if (-Not (Test-Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam\")) {
+        $startTime = Get-Date
+        $url = "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
+        $tempDir = "C:\Temp"
+        $outputFile = "C:\Temp\SteamSetup.exe"
+
+        if ( !(Test-Path $tempDir) ) {
+            Write-Output "The temp directory doesn't exist. Creating $tempDir"
+            New-Item -Path $tempDir -ItemType Directory
+        }
+
+        Push-Location $tempDir
+
+        Write-Output "Downloading Steam install executable from $url"
+        $webClient = New-Object System.Net.WebClient
+        $webClient.DownloadFile($url, $outputFile)
+
+        Pop-Location
+
+        Write-Output "Running the installation executable $outputFile"
+        try {
+            & $outputFile /S
+        } catch {
+            Write-Warning "Steam install failed when executing: $outputFile /S"
+        }
+
+        if (Test-Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam\") {
+            Write-Output "Steam Successfully installed!!!  Removing installation executable."
+            Remove-Item $outputFile -Force
+        }
+
+        Write-Output "Time taken $((Get-Date).Subtract($startTime).Seconds) second(s)"
+
+    } else {
+        Write-Output "Steam is already installed on this machine. Nothing to do."
+    }
+
+}
+
+Install-Steam
---
+++ Install-VSCode.ps1
@@ -0,0 +1,48 @@
+﻿function Install-VSCode {
+    param(
+    )
+    if (-Not (Test-Path "C:\Program Files\Microsoft VS Code\Code.exe")) {
+        $startTime = Get-Date
+        $url = "https://go.microsoft.com/fwlink/?Linkid=852157"
+        $tempDir = "C:\Temp"
+        $outputFile = "C:\Temp\VSCode.exe"
+
+        if ( !(Test-Path $tempDir) ) {
+            Write-Output "The temp directory doesn't exist. Creating $tempDir"
+            New-Item -Path $tempDir -ItemType Directory
+        }
+
+        Push-Location $tempDir
+
+        Write-Output "Downloading VSCode install executable from $url"
+        $webClient = New-Object System.Net.WebClient
+        $webClient.DownloadFile($url, $outputFile)
+
+        Pop-Location
+
+        Write-Output "Running the installation executable $outputFile"
+        try {
+            Start-Process $outputFile -ArgumentList "/verysilent /norestart /MERGETASKS=!runcode" -wait
+        } catch {
+            Write-Warning "Steam install failed when executing: $outputFile /VERYSILENT /MERGETASKS=!runcode"
+        }
+
+        if (Test-Path "C:\Program Files\Microsoft VS Code\Code.exe") {
+            Write-Output "Steam Successfully installed!!!  Removing installation executable."
+            Remove-Item $outputFile -Force
+
+            #Write-Verbose "Adding VSCode to the System level Path Environment Variable."
+            #$oldPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+            #$newPath = "$oldPath;C:\Program Files\Microsoft VS Code\;"
+            #Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+        }
+
+        Write-Output "Time taken $((Get-Date).Subtract($startTime).Seconds) second(s)"
+
+    } else {
+        Write-Output "VSCode is already installed on this machine. Nothing to do."
+    }
+
+}
+
+Install-VSCode
